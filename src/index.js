23cc4be4cea003ab4d918c7d497d69ecfb41e608
Chris Fregly <dev@chrisfregly.com>
2023-05-30 17:50:07 -0500

    add van conversion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
+import Home from './Home';
+import VanConversion from './VanConversion';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="van-conversion" element={<VanConversion />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
 );
