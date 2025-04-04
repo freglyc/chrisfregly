79f4b5da03d8ca97f7acf3c6d4c208ae7f2dddcf
Chris Fregly <pro@chrisfregly.com>
2025-04-03 18:10:41 -0700

    streamline photo sharing

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import Home from './Home';
-import VanConversion from './VanConversion';
-import TheRoad from './TheRoad';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
       <Routes>
-        <Route path="van-conversion" element={<VanConversion />} />
-        <Route path="the-road" element={<TheRoad />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
